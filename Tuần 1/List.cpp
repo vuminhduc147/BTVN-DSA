e6d11013fe703955ad0887dce42518d77ec573f8
vuminhduc147 <129876913+vuminhduc147@users.noreply.github.com>
2025-09-29 22:42:31 +0700

    Update List.cpp

--- Tuần 1/List.cpp
+++ Tuần 1/List.cpp
@@ -6,106 +6,109 @@ using namespace std;
 struct List {
     int a[MAX];
     int n; // số phần tử hiện tại
+
+    // Khởi tạo
+    List() { n = 0; }
+
+    // Truy cập
+    int get(int i) {
+        if (i < 0 || i >= n) return -1;
+        return a[i];
+    }
+
+    // Chèn đầu
+    void insertFirst(int x) {
+        for (int i = n; i > 0; i--) a[i] = a[i-1];
+        a[0] = x;
+        n++;
+    }
+
+    // Chèn cuối
+    void insertLast(int x) {
+        a[n++] = x;
+    }
+
+    // Chèn tại i
+    void insertAt(int i, int x) {
+        if (i < 0 || i > n) return;
+        for (int j = n; j > i; j--) a[j] = a[j-1];
+        a[i] = x;
+        n++;
+    }
+
+    // Xóa đầu
+    void deleteFirst() {
+        if (n == 0) return;
+        for (int i = 0; i < n-1; i++) a[i] = a[i+1];
+        n--;
+    }
+
+    // Xóa cuối
+    void deleteLast() {
+        if (n > 0) n--;
+    }
+
+    // Xóa tại i
+    void deleteAt(int i) {
+        if (i < 0 || i >= n) return;
+        for (int j = i; j < n-1; j++) a[j] = a[j+1];
+        n--;
+    }
+
+    // Duyệt xuôi
+    void traverseForward() {
+        for (int i = 0; i < n; i++) cout << a[i] << " ";
+        cout << endl;
+    }
+
+    // Duyệt ngược
+    void traverseBackward() {
+        for (int i = n-1; i >= 0; i--) cout << a[i] << " ";
+        cout << endl;
+    }
 };
-
-// Truy cập
-int get(List L, int i) {
-    if (i < 0 || i >= L.n) return -1;
-    return L.a[i];
-}
-
-// Chèn đầu
-void insertFirst(List &L, int x) {
-    for (int i = L.n; i > 0; i--) L.a[i] = L.a[i-1];
-    L.a[0] = x;
-    L.n++;
-}
-
-// Chèn cuối
-void insertLast(List &L, int x) {
-    L.a[L.n++] = x;
-}
-
-// Chèn tại i
-void insertAt(List &L, int i, int x) {
-    if (i < 0 || i > L.n) return;
-    for (int j = L.n; j > i; j--) L.a[j] = L.a[j-1];
-    L.a[i] = x;
-    L.n++;
-}
-
-// Xóa đầu
-void deleteFirst(List &L) {
-    for (int i = 0; i < L.n-1; i++) L.a[i] = L.a[i+1];
-    L.n--;
-}
-
-// Xóa cuối
-void deleteLast(List &L) {
-    if (L.n > 0) L.n--;
-}
-
-// Xóa tại i
-void deleteAt(List &L, int i) {
-    if (i < 0 || i >= L.n) return;
-    for (int j = i; j < L.n-1; j++) L.a[j] = L.a[j+1];
-    L.n--;
-}
-
-// Duyệt xuôi
-void traverseForward(List L) {
-    for (int i = 0; i < L.n; i++) cout << L.a[i] << " ";
-    cout << endl;
-}
-
-// Duyệt ngược
-void traverseBackward(List L) {
-    for (int i = L.n-1; i >= 0; i--) cout << L.a[i] << " ";
-    cout << endl;
-}
 
 int main() {
     List L;
-    L.n = 0;
 
     // Chèn cuối
-    insertLast(L, 10);
-    insertLast(L, 20);
-    insertLast(L, 30);
+    L.insertLast(10);
+    L.insertLast(20);
+    L.insertLast(30);
     cout << "Sau khi chèn cuối 10,20,30: ";
-    traverseForward(L);
+    L.traverseForward();
 
     // Chèn đầu
-    insertFirst(L, 5);
+    L.insertFirst(5);
     cout << "Sau khi chèn đầu 5: ";
-    traverseForward(L);
+    L.traverseForward();
 
     // Chèn tại vị trí i
-    insertAt(L, 2, 15);
+    L.insertAt(2, 15);
     cout << "Sau khi chèn 15 vào vị trí 2: ";
-    traverseForward(L);
+    L.traverseForward();
 
     // Xóa đầu
-    deleteFirst(L);
+    L.deleteFirst();
     cout << "Sau khi xóa đầu: ";
-    traverseForward(L);
+    L.traverseForward();
 
     // Xóa cuối
-    deleteLast(L);
+    L.deleteLast();
     cout << "Sau khi xóa cuối: ";
-    traverseForward(L);
+    L.traverseForward();
 
     // Xóa tại vị trí i
-    deleteAt(L, 1);
+    L.deleteAt(1);
     cout << "Sau khi xóa tại vị trí 1: ";
-    traverseForward(L);
+    L.traverseForward();
 
     // Duyệt ngược
     cout << "Duyệt ngược: ";
-    traverseBackward(L);
+    L.traverseBackward();
 
     // Truy cập
-    cout << "Phần tử tại vị trí 0 là: " << get(L, 0) << endl;
+    cout << "Phần tử tại vị trí 0 là: " << L.get(0) << endl;
 
     return 0;
 }
